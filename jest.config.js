6dfbad961136413cb42defe286ce773c89ba6d67
Jady Rufino <jady.rufino@staples.ca>
2024-06-03 18:28:44 -0300

    chore: set up test configuration

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+};
